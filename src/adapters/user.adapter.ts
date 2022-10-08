1167a5c6f53bcbc95a8bdd2a416db61fb4b560a5
cesarcr300 <cc30122003@gmail.com>
2022-10-07 23:40:54 -0500

    modify userAdapter

--- src/adapters/user.adapter.ts
+++ src/adapters/user.adapter.ts
@@ -1,5 +1,7 @@
-export const createUserAdapter = (user: any) => ({
-  name: user.data.name,
-  gender: user.data.gender,
-  status: user.data.status
+import { IUser } from "@/models";
+
+export const createUserAdapter = (user: any): IUser => ({
+  name: user.name as string,
+  lastName: user.lastName as string,
+  userId: user.idUser as number
 });

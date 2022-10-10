3a7479307e7260cd232681853b7a1e992d0f3281
cesarcr300 <cc30122003@gmail.com>
2022-10-09 20:27:47 -0500

    create income implemented

--- src/pages/Incomes/components/IncomesCreationForm.component.tsx
+++ src/pages/Incomes/components/IncomesCreationForm.component.tsx
@@ -4,8 +4,12 @@ import { Select } from "@/components";
 import { useForm } from "@/hooks"
 import { ISelectOption, IIncomeCreation } from "@/models";
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router";
+import { createIncome } from "@/pages";
 
 export const IncomesCreationForm = () => {
+    const navigate = useNavigate();
+
     const { formValues, handleInputChange, handleSelectChange } = useForm<IIncomeCreation>({
         amount: 0,
         description: "",
@@ -20,8 +24,16 @@ export const IncomesCreationForm = () => {
         getCategoriesToSelect(1, setCategoriesOptions);
         getAccountsToSelect(1, setAccountsOptions);
     }, []);
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const wasCreated = createIncome(1, formValues);
+        if (!wasCreated) return;
+        console.log('se creo');
+        navigate("/incomes");
+    }
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <div>
                 <input type="number" name="amount" value={formValues.amount} onChange={handleInputChange} />
                 <input name="description" type="text" value={formValues.description} onChange={handleInputChange} />

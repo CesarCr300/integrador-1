c9718f80b5007d13c7600df04cd26390a18180e0
cesarcr300 <cc30122003@gmail.com>
2022-12-06 19:58:03 -0500

    hotfix

--- src/adapters/general-information.adapter.ts
+++ src/adapters/general-information.adapter.ts
@@ -1,7 +1,7 @@
 import { IGeneralInformation } from "@/models";
 
 export const createGeneralInformationAdapter = (data: any): IGeneralInformation => {
-    const accounts = data?.accounts.map((account: any) => {
+    const accounts = data?.accounts?.map((account: any) => {
         return { name: account.nameAccount, amount: account.amountAccount };
     });
     return { accounts, incomesAmount: data.income, expensesAmount: data.expenses };

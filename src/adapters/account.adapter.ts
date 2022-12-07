8e3cc48b73a3ce8c173b60e735d9e25a5d367c86
cesarcr300 <cc30122003@gmail.com>
2022-12-06 20:17:44 -0500

    hotfix

--- src/adapters/account.adapter.ts
+++ src/adapters/account.adapter.ts
@@ -1,7 +1,6 @@
 import { IAccount } from "@/models";
 
 export const createAccountAdapter = (data: any): IAccount[] => {
-    console.log(data);
     let response = data?.map((account: any): IAccount => {
         return { name: account.nameAccount, id: account.idAccount };
     });
--- src/adapters/expense.adapter.ts
+++ src/adapters/expense.adapter.ts
@@ -1,21 +1,25 @@
 import { IExpenseCategory, IExpenseDetail, IExpenses } from "@/models";
 
 export const createExpensesAdapter = (data: any): IExpenses => {
+    let categories = data.category?.map((category: any): IExpenseCategory => {
+        return {
+            name: category.nameCategory,
+            amount: category.amountCategory
+        }
+    });
+    let details = data.detail?.map((detail: any): IExpenseDetail => {
+        return {
+            description: detail.descriptionDetail,
+            amount: detail.amountDetail,
+            id: detail.idDetail,
+            categoryName: detail.categoryName
+        }
+    })
+    if (categories == null) categories = [];
+    if (details == null) details = []
     return {
         amount: data.expenses,
-        categories: data.category?.map((category: any): IExpenseCategory => {
-            return {
-                name: category.nameCategory,
-                amount: category.amountCategory
-            }
-        }),
-        details: data.detail?.map((detail: any): IExpenseDetail => {
-            return {
-                description: detail.descriptionDetail,
-                amount: detail.amountDetail,
-                id: detail.idDetail,
-                categoryName: detail.categoryName
-            }
-        })
+        categories,
+        details
     }
 }

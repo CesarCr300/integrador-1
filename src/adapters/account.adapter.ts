9cd066e1cd39c9266983775b149e79322738cc96
cesarcr300 <cc30122003@gmail.com>
2022-12-06 20:14:26 -0500

    hotfix

--- src/adapters/account.adapter.ts
+++ src/adapters/account.adapter.ts
@@ -2,7 +2,7 @@ import { IAccount } from "@/models";
 
 export const createAccountAdapter = (data: any): IAccount[] => {
     console.log(data);
-    let response = data?.accounts?.map((account: any): IAccount => {
+    let response = data?.map((account: any): IAccount => {
         return { name: account.nameAccount, id: account.idAccount };
     });
     if (response == null) response = [];
